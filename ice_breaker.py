f85c1273442209877e346dcdcc83ce99354c5bef
ot-babs <106931726+ot-babs@users.noreply.github.com>
2023-12-18 23:14:18 +0000

    added output parser

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,13 +6,10 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
+from output_parsers import person_intel_parser, PersonIntel
 
-name = "johnlegend"
-
-if __name__ == "__main__":
-    print("Hello LangChain!")
-
-    linkedin_profile_url = linkedin_lookup_agent(name="Steven Bartlett")
+def ice_break(name: str) -> PersonIntel:
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
     twitter_username = twitter_lookup_agent(name=name)
@@ -24,17 +21,25 @@ if __name__ == "__main__":
            1. a short summary
            2. two interesting facts about them
            3. A topic that may interest them
-           4. 2 creative ice breakers to open a conversation with them
-           5. Write a note on why I wish to connect. In our case for them help us get a job at their company.
+           4. 2 creative ice breakers to open a conversation with 
+                    \n{format_instructions}
     """
 
     summary_template_template = PromptTemplate(
         input_variables=["linkedin_information", "twitter_information"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_template_template)
 
-    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
+    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
+    return person_intel_parser.parse(result)
+
+if __name__ == "__main__":
+    print("Hello LangChain!")
+    result = ice_break(name="Steven Bartlett")
